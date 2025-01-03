66aee1a46baa559652b4c04ed9f04705cc34d95d
itsTony4dev <tonyayda7@gmail.com>
2025-01-03 16:48:27 +0200

    minor fixes

--- history.php
+++ history.php
@@ -49,50 +49,51 @@ $page = $_GET['page'];
             </tbody>
         </table>
         <div class="back-button">
-   <a href="<?= $page ?? 'home' ?>.php"><i class="fa-solid fa-left-long fa-xl"></i> Back</a>
-</div>
-</div>
+            <a href="<?= $page ?? 'home' ?>.php"><i class="fa-solid fa-left-long fa-xl"></i> Back</a>
+        </div>
+    </div>
 
-<div class="modal-overlay" id="purchaseHistoryModal">
-   <div class="details-container">
-       <div class="details-header">
-           <h2>Order Details</h2>
-           <button class="close-button" onclick="closeModal()">Close</button>
-       </div>
-       <ul class="product-list" id="modalProductList">
-       </ul>
-   </div>
-</div>
+    <div class="modal-overlay" id="purchaseHistoryModal">
+        <div class="details-container">
+            <div class="details-header">
+                <h2>Order Details</h2>
+                <button class="close-button" onclick="closeModal()">Close</button>
+            </div>
+            <ul class="product-list" id="modalProductList">
+            </ul>
+        </div>
+    </div>
 
-<script>
-function closeModal() {
-   document.getElementById('purchaseHistoryModal').style.display = 'none';
-}
+    <script>
+        function closeModal() {
+            document.getElementById('purchaseHistoryModal').style.display = 'none';
+        }
 
-function openModal(orderId) {
-   fetch('getOrderItems.php?order_id=' + orderId)
-       .then(response => response.json())
-       .then(data => {
-           updateModalContent(data);
-           document.getElementById('purchaseHistoryModal').style.display = 'flex';
-       });
-}
+        function openModal(orderId) {
+            fetch('getOrderItems.php?order_id=' + orderId)
+                .then(response => response.json())
+                .then(data => {
+                    updateModalContent(data);
+                    document.getElementById('purchaseHistoryModal').style.display = 'flex';
+                });
+        }
 
-function updateModalContent(data) {
-   const productList = document.getElementById('modalProductList');
-   productList.innerHTML = data.items.map(item => `
+        function updateModalContent(data) {
+            const productList = document.getElementById('modalProductList');
+            productList.innerHTML = data.items.map(item => `
        <li>
            <div class="product-info">
                <img src="uploads/${item.image}" alt="Product Image" class="product-image">
                <div>
                    <div class="product-name">${(item.name).trim() == '' ? 'Gaming Pc' : item.name}</div>
                    <div class="product-price">$${item.price}</div>
-               </div>
            </div>
            <div class="product-quantity">Quantity: ${item.quantity}</div>
        </li>
    `).join('');
-}
-</script>
+        }
+    </script>
 </body>
+</div>
+
 </html>
